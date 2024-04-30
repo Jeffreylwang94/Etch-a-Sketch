88f4a12bd1c997f474837a8215169cbf354bb875
Jeffreylwang94 <jefflwang94@gmail.com>
2024-04-30 10:51:34 -0700

    Added version for opacity selection

---
+++ javascript_opacity.js
@@ -0,0 +1,73 @@
+let initialNumberOfSides = 16;
+
+const parentContainer = document.querySelector(".container");
+containerHeight = parentContainer.offsetHeight;
+containerWidth = parentContainer.offsetWidth;
+
+function colorRandomizer(){
+    return "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
+}
+
+function createGrid(numSideSquares){
+    clearGrid();
+    for (let i = 0; i < numSideSquares*numSideSquares; i++){
+        let square = document.createElement("div");
+        square.style.width = containerHeight/numSideSquares+"px";
+        square.style.height = containerWidth/numSideSquares+"px";
+        square.style.backgroundColor = "black";
+        // square.style.backgroundColor = colorRandomizer();
+        square.classList.add("square");
+        // square.style.border = "2px solid red";
+        parentContainer.append(square);
+        square.addEventListener("mouseenter",(e) => {
+            // if (square.style.backgroundColor == "black"){
+                square.style.backgroundColor = colorRandomizer();
+            // } else {
+                // square.style.backgroundColor = "black";
+            }
+        )
+    }
+}
+
+function createGrid_less_opacity(numSideSquares){
+    clearGrid();
+    parentContainer.style.backgroundColor = "black";
+    for (let i = 0; i < numSideSquares*numSideSquares; i++){
+        let square = document.createElement("div");
+        square.style.width = containerHeight/numSideSquares+"px";
+        square.style.height = containerWidth/numSideSquares+"px";
+        square.style.backgroundColor = "white";
+        square.style.opacity = 1;
+        square.classList.add("square");
+        // square.style.border = "2px solid red";
+        parentContainer.append(square);
+        square.addEventListener("mouseenter",(e) => {
+            square.style.opacity = square.style.opacity-0.1;
+            }
+        )
+    }
+}
+
+function clearGrid(){
+    while(parentContainer.firstChild){
+        parentContainer.removeChild(parentContainer.lastChild);
+    }
+}
+
+
+
+const resetButton = document.querySelector("button");
+const inputValue = document.querySelector("input");
+
+// createGrid(initialNumberOfSides);
+createGrid_less_opacity(initialNumberOfSides);
+
+resetButton.addEventListener("click", () => {
+    if (inputValue.value > 100 || inputValue.value < 0) {
+        alert("Please enter a positive value less than or equal to 100");
+    } else {
+        // createGrid(inputValue.value);
+        createGrid_less_opacity(inputValue.value);
+    }
+})
+
